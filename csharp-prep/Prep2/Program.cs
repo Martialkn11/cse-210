b51b8b47a84c4a85fdbc8278f02718024503015f
Martial N'guessan <nguessanmartial433@gmail.com>
2024-06-19 21:57:23 +0000

    commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        
+        int percent = int.Parse(answer);
+
+        string note ="";
+
+        if(percent == 20)
+        {
+            note = "Good";
+        }
+        
+        else if(percent >=10)
+        {
+            note = "Enough good";
+        }
+
+        else
+        {
+            note = "Bad";
+        }
+
+        Console.WriteLine($"Your grade is: {note}");
+
+        if(percent>=10)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else if(percent<10)
+        {
+            Console.WriteLine("You do not have a good note so you do not pass.");
+        }
     }
 }
